22b757cee044e307b993af0c7b8f2bdb19184316
Vincent Ace Fronteras <vincentace06@gmail.com>
2026-03-15 22:08:51 +0800

    header progress

---
+++ components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <div className="text- font-bold p-4 border-b border-gray-300">
+          Med App Reminder
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
+
