602ec9dcf13222e36fc49d8ca0f107d8a1a66d4c
Kmontajas <Montajaskristinemae@gmail.com>
2026-05-15 00:56:49 +0800

    chore: remove unused React import

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
